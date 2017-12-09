2a83afd84b6421acb46f58c3334aa12451c12754
damianx93 <33983206+damianx93@users.noreply.github.com>
2017-12-09 16:01:08 +0100

    update

--- src/GameLogic/GameController.java
+++ src/GameLogic/GameController.java
@@ -1,10 +1,16 @@
 package GameLogic;
 
+import com.sun.xml.internal.fastinfoset.util.StringArray;
 import javafx.beans.property.SimpleStringProperty;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class GameController {
+    //private StringArray inputMessage;
     private GameModel gameModel;
     private SimpleStringProperty gameResponseMessage;
+    private String[] inputMessage;
 
     public GameController(GameModel gameModel) {
 
@@ -13,4 +19,32 @@ public class GameController {
     }
 
     public void startGame() { gameModel.init(); }
+
+    public void readMessage(String message) {
+        ArrayList<String> firstSplit = splitMessage(message,"@");
+        ArrayList<ArrayList<String>> secondSplit = new ArrayList<>();
+        ArrayList<ArrayList<String>> thirdSplit = new ArrayList<>();
+
+        for (int i = 0; i <= firstSplit.size(); i++){
+            secondSplit.add(i,splitMessage(firstSplit.get(i),"/"));
+
+            switch(secondSplit.get(i).get(0)){
+
+                case "playTreasure": gameModel.playTreasures();
+                                    break;
+
+                case "endTurn":
+
+                case "buy": gameModel.buyCard(secondSplit.get(i).get());
+            }
+
+
+
+        }
+    }
+
+    private ArrayList<String> splitMessage(String message, String delimiter) {
+        ArrayList<String> inputMessage = new ArrayList<>(Arrays.asList(message.split(delimiter)));
+        return inputMessage;
+    }
 }
