f1b7ac7b6cc4dffa7bdfcb9337eecf5253a1f3fd
damianx93 <33983206+damianx93@users.noreply.github.com>
2017-12-16 16:55:21 +0100

    update

--- src/GameLogic/Game.java
+++ src/GameLogic/Game.java
@@ -13,16 +13,32 @@ public class Game extends Observable{
     private SimpleStringProperty gameResponseMessage;
     private String responseMessage;
     private String inputMessage;
+    private int cardsInGame;
+    private String[] playerNames;
+
 
 
     public Game(String gameName, int cardsInGame, String... playerNames) {
+        this.cardsInGame = cardsInGame;
+        this.playerNames = playerNames;
         responseMessage= new String();
-        //this.gameModel = new GameModel(gameName, gameResponseMessage);
-        this.gameController = new GameController(gameModel);
+        this.gameModel = new GameModel(gameName, gameResponseMessage);
+        this.gameController = new GameController(gameModel,this);
         this.gameResponseMessage = new SimpleStringProperty(); //gaht das??
     }
 
-    public void startGame() { gameController.startGame();    }
+    public void createGame(String[] playerNames ){
+        int playerCount = playerNames.length;
+
+        for ( int i = 0 ; i < playerNames.length; i++){
+            gameModel.addPlayer(playerNames[i],playerCount);
+        }
+
+    }
+
+    public void startGame(int cardsInGame) {
+        createGame(playerNames);
+        gameController.startGame(cardsInGame);    }
 
     public String getGameName() {
         return gameModel.getGameName();
@@ -31,6 +47,7 @@ public class Game extends Observable{
     public void readMessage(String message) {
        // gameController.readMessage(message)
         this.inputMessage=message;
+        gameController.readMessage(message);
     }
 
     public void setResponseMessage(String responseMessage){
